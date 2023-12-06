cae9250e9cf134b42e4ef628c66bdb0a7891097d
Chris <chris@pop-os.localdomain>
2023-12-05 22:36:18 -0800

    12/5/2023 NIGHT

--- Scenes/ALPHA/AlphaFarmers/FarmerJoe/farmer_joe.gd
+++ Scenes/ALPHA/AlphaFarmers/FarmerJoe/farmer_joe.gd
@@ -2,12 +2,13 @@ extends Area2D
 # FARMER JOE
 # An NPC test man named Farmer Joe. He is a modest farmer of a small modest farm.
 # **********************
-# PLAN
+# THE PLAN
 # **********************
 # The farm/area 'controller' will spawn an NPC, and give it a name and family, possibly. The controller will
 # then give the NPC a schedule, temperment, and other characteristics that will govern the NPC's personality. 
 var temprement = "EASY"
-var schedule = {} # schedule dictionary
+var schedule = {"winter": {},"spring": {},"summer": {},"fall": {}} # schedule dictionary
 var money = [20,0,0] # the funds for farmer joe [copper, silver, gold]
 var food_stores = 80 # max 100 (gets food
 var water_stores = 80 # max 100
+var current_task # the current task for Farmer Joe
