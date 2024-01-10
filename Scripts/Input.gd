b357a24e35919e5a62dd2a9f1113477548e4fa98
Christopher Glenn <topherblair@gmail.com>
2024-01-10 14:34:15 -0800

    1/10/2024 AFTERNOON

--- Scripts/Input.gd
+++ Scripts/Input.gd
@@ -2,3 +2,6 @@ extends Node
 # INPUT
 # provides input for the game based on the current control settings all scripts are here
 # so they don't have to be littered all around the game
+
+func player_input(mode, key):
+	pass
