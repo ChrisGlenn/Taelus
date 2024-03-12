a791abad617fbb16d13bf2f08b4e8a1a023d5de0
Chris <topherblair@gmail.com>
2024-03-12 10:42:52 -0700

    3/12/2024 MORNING

--- Scenes/Cells/Region-Lor/L00x48/lor_00x_48.gd
+++ Scenes/Cells/Region-Lor/L00x48/lor_00x_48.gd
@@ -1,7 +1,20 @@
 extends Node2D
 # CELL 00X48 IN REGION LOR
+@onready var ROOF = $Roofs
+@onready var BLACKOUT = $BlackOut
 @export_multiline var description = "" # used for if the player looks around
+
 
 func _ready():
 	Globals.current_region = "Lor" # set current region
 	Globals.current_location = "Watchtower" # set current location
+
+func _process(_delta):
+	# check if the player is inside of an interior (the guardtower) and then show/hide the
+	# roof and blackout depending on the Globals interior status
+	if Globals.interior:
+		ROOF.visible = false # hide the roof
+		BLACKOUT.visible = true # show the blackout
+	else:
+		ROOF.visible = true # show the roof
+		BLACKOUT.visible = false # hide the blackout
--- Scenes/System/InvisibleBlock/invisible_block.gd
+++ Scenes/System/InvisibleBlock/invisible_block.gd
@@ -1,11 +1,30 @@
 extends Area2D
 # INVISIBLE BLOCK
 # used to block off the player
-# if this is set 'border' to true then it will display a message telling the player that
-# they can go no further
+# if this is set 'border' to true then it will display a message telling the player that they can go no further
+# if set to 'interior' then it will set the player's interior status to true but false if exited...
 @export var border = false
+@export var interior = false
+
+
+func _ready():
+	if interior:
+		# turn off collision
+		set_collision_layer_value(1, false)
+		set_collision_mask_value(1, false)
+		# turn on collision for interior
+		set_collision_layer_value(9, true)
+		set_collision_mask_value(9, true)
 
 
 func _on_body_entered(body):
 	if body.is_in_group("PLAYER"):
-		Functions.message("You can go no further...")
+		if border:
+			Functions.message("You can go no further...")
+		if interior:
+			Globals.interior = true # set interior to true
+
+func _on_body_exited(body:Node2D):
+	if body.is_in_group("PLAYER"):
+		if interior:
+			Globals.interior = false # set interior to false
