959e072a5c781b8bf3d3c0524c85c9ff5b8ee885
Christopher Glenn <topherblair@gmail.com>
2024-05-22 15:52:47 -0700

    5/22/2024 AFTERNOON

--- Scenes/Prefabs/Enviornment/Tree/tree.gd
+++ Scenes/Prefabs/Enviornment/Tree/tree.gd
@@ -2,15 +2,16 @@ extends StaticBody2D
 # TREE SCRIPT
 # a script for the variety of trees in Taelus (starting in the Lor region)
 # see documentation for various tree types lifespan/ect.
-var tree_id = "" # the ID of the tree
-var tree_recorded = false # if true then this tree has a listing in the Globals.tree array
-var tree_species = "chestnut" # correlates to the 'animation'
-var tree_age = 0 # age in months
+@onready var ANIM_SPRITE = $AnimatedSprite2D
+@export var tree_id = "" # the ID of the tree
+@export_enum("chestnut", "apple") var tree_species = "chestnut" # correlates to the 'animation'
+@export var tree_age = 0 # age in months
+@export var will_seed = true # if the tree can seed new trees around it during Spring months
+@export var seed_age = 2 # set to years
+@export var cut_down = false # if the tree has been cut down
+@export var dead = false # if the tree is dead or not
 var tree_life_milestones = [16,800] # goes from small to fully grown, goes into dead
 var tree_seasonal_update = [3,1,1,4] # winter, spring, summer, fall the frames for each season (default chestnut)
-var will_seed = true # if the tree can seed new trees around it during Spring months
-var seed_age = 2 # set to years
-var cut_down = false # if the tree has been cut down
 var month_check # used to check the passage of months
 var year_check # used to check the passage of years
 
@@ -20,11 +21,30 @@ func _ready():
 	# from there and if not then from the set variables
 	if Globals.trees.size() > 0:
 		pass
-	if !tree_recorded:
-		# load the tree based on the variables set by the scene this spawned into
+	else:
 		pass
 	month_check = Globals.month # set to current month
 	year_check = Globals.year # set to current year
+	ANIM_SPRITE.play(tree_species) # set animation based off species
+
 
 func _process(_delta):
+	tree_aging() # tree growth/aging function
+
+
+func tree_aging():
+	# check against the current season and change
+	# check months/years and compare it to the set milestones
+	if month_check != Globals.month:
+		tree_age += 1 # increment age (recorded in months)
+	if tree_age < tree_life_milestones[0]:
+		if !dead: ANIM_SPRITE.frame = 0 # baby tree :D
+		else: ANIM_SPRITE.frame = 2 # dead baby tree :(
+	elif tree_age >= tree_life_milestones[0] and tree_age < tree_life_milestones[1]:
+		ANIM_SPRITE.frame = 1 # tree
+	elif tree_age >= tree_life_milestones[1]:
+		ANIM_SPRITE.frame = 3 # dead adult
+
+func record_tree():
+	# record the tree into the Globals if not recorded
 	pass
